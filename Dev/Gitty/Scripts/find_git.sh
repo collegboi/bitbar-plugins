0d3272583d52ededf9ada2256fd9fac02edffee0
Timothy Barnard <test>
2017-07-16 15:41:08 +0100

    gitty updates

--- Dev/Gitty/Scripts/find_git.sh
+++ Dev/Gitty/Scripts/find_git.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# <bitbar.title>Gitty</bitbar.title>
-# <bitbar.version>v0.1</bitbar.version>
-# <bitbar.author>Timothy Barnard</bitbar.author>
-# <bitbar.author.github>collegboi</bitbar.author.github>
-# <bitbar.image>https://raw.githubusercontent.com/collegboi/Bitbar-Gitty/master/image1.png</bitbar.image>
-# <bitbar.desc>Shows the current status of local repos</bitbar.desc>
-
 if [ "$1" = "copy" ]; then
     echo "$2" | pbcopy
 fi
@@ -17,4 +10,8 @@ if [ "$1" = "run" ]; then
         mkdir "$REPO_DIR"
     fi
     find "$V_MY_PATH/Documents" -name .git -type d -prune -not -path '*/.*/*' > "$V_MY_PATH/Documents/MacBarPlugins/Files/repos.txt"
+fi
+
+if [ "$1" = "open" ]; then
+   open "$2"
 fi
